a466a647125ec03f82eef4713b6ac4e3c3fb22e7
giusers <yufreebies@gmail.com>
2023-12-16 16:05:43 +1030

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,13 @@
 <?php
+function ntp($server,$port=123){
+  echo $server;
+  $socket=@fsockopen("udp://$server",$port,$err_no,$err_str,1);
+  if(!$socket)return;
+  fwrite($socket,chr(0x1b).str_repeat("\0",47));
+  $packetReceived=fread($socket,48);
+  $unixTimestamp=unpack('N',$packetReceived,40)[1]-2208988800;
+  $utcDate=date("Y-m-d H:i:s",$unixTimestamp);
+  echo ":<b>$utcDate</b><br/>\n";
+}
+ntp('time.cloudflare.com');
 phpinfo();
