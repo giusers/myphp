a4dabb903e80ef3baf87bd2d91bac1e58f079c8e
giusers <yufreebies@gmail.com>
2023-12-16 16:16:26 +1030

    Update index.php

--- api/index.php
+++ api/index.php
@@ -5,6 +5,7 @@ function ntp($server,$port=123){
   if(!$socket)return;
   fwrite($socket,chr(0x1b).str_repeat("\0",47));
   $packetReceived=fread($socket,48);
+  echo $packetReceived;
   $unixTimestamp=unpack('N',$packetReceived,40)[1]-2208988800;
   $utcDate=date("Y-m-d H:i:s",$unixTimestamp);
   echo ":<b>$utcDate</b><br/>\n";
